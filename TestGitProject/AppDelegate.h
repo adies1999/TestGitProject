e09cc0423277ceee617de80d1e130fd52197320a
adies1999 <adies1999@hotmail.com>
2012-09-01 11:55:18 +0700

    First Commit

---
+++ TestGitProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestGitProject
+//
+//  Created by adies on 9/1/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestGitProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestGitProject
+//
+//  Created by adies on 9/1/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
